b229f50138cc3390219e05c1f3234825dd99be39
tthompson082 <tthompson082@gmail.com>
2019-07-22 15:44:54 -0400

    :label: Create Handlers, Exceptions, and Error Detail
    Create Handlers package, Exceptions package, and ErrorDetail Class for exception handling

---
+++ school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.school.handlers;
+
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class RestExceptionHandler extends ResponseEntityExceptionHandler
+{
+}
