0e5cc3d149086c7c8555465f7b70bfe52a8c1398
tthompson082 <tthompson082@gmail.com>
2019-07-22 16:45:08 -0400

    :sparkles: Create Exception for No Handler or Wrong Type

--- school/src/main/java/com/lambdaschool/school/SchoolApplication.java
+++ school/src/main/java/com/lambdaschool/school/SchoolApplication.java
@@ -2,14 +2,21 @@ package com.lambdaschool.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class SchoolApplication
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(SchoolApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SchoolApplication.class, args);
+
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 
 }
--- school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
+++ school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
@@ -3,10 +3,14 @@ package com.lambdaschool.school.handlers;
 
 import com.lambdaschool.school.exceptions.ResourceNotFoundException;
 import com.lambdaschool.school.model.ErrorDetail;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +29,32 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler
         errorDetail.setTitle("Resource Not Found");
         errorDetail.setDetail(rnfe.getMessage());
         errorDetail.setDeveloperMessage(rnfe.getClass().getName());
+
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorDetail.setTitle(ex.getPropertyName());
+        errorDetail.setDetail(ex.getMessage());
+        errorDetail.setDeveloperMessage(request.getDescription(true));
+
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle(ex.getRequestURL());
+        errorDetail.setDetail(request.getDescription(true));
+        errorDetail.setDeveloperMessage("Rest Handler Not Found (check for valid URI)");
 
         return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
     }
