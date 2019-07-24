72b3ac6b7df278e56760cc0f60eb3e11376b7a0a
tthompson082 <tthompson082@gmail.com>
2019-07-24 16:19:47 -0400

    :ambulance: Create Test for Response Time

---
+++ school/src/test/java/com/lambdaschool/school/controller/CourseControllerIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.lambdaschool.school.controller;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.number.OrderingComparison.lessThan;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CourseControllerIntegrationTest
+{
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void initialiseRestAssuredMockMvcWebApplicationContext()
+    {
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+    }
+
+    @Test
+    public void whenMeasuredResponseTime()
+    {
+        given().when().get("/courses/courses").then().time(lessThan(5000L));
+    }
+}
