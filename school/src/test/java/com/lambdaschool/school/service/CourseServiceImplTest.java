c65e23f5098de0128c81d78c29d327ac28bf7fea
tthompson082 <tthompson082@gmail.com>
2019-07-24 15:15:49 -0400

    :fire: Create GetCourseById service and test

---
+++ school/src/test/java/com/lambdaschool/school/service/CourseServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.lambdaschool.school.service;
+
+import com.lambdaschool.school.SchoolApplication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SchoolApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class CourseServiceImplTest
+{
+    @Autowired
+    private CourseService courseService;
+
+    @Before
+    public void AsetUp() throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @After
+    public void BtearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void CfindCourseById()
+    {
+        assertEquals("Data Science", courseService.findCourseById(1).getCoursename());
+    }
+}
